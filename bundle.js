c746c524d4f6b767de614744e8b12fbcc8fc6d62
Kawandeep Virdee <whichlight@gmail.com>
2013-08-28 10:59:19 -0400

    added array processing step

--- bundle.js
+++ bundle.js
@@ -13,7 +13,7 @@ function getDataArray(bytearray){
   var dataArray = createArray(width,height);
   for(var i=0;i<bytearray.length/2;i++){
     //for depth feed  . bytearray  [val , mult, val2, mult2, ...]
-    // when out of range, mult=7
+    // when out of range, mult=7. range (0-255, 0-7)
     var depthVal = bytearray[2*i]+bytearray[2*i+1]*255;
     var x = i % width;
     var y = Math.floor(i / width);
@@ -22,12 +22,12 @@ function getDataArray(bytearray){
   return dataArray;
 }
 
-function renderCanvas(dataArray){
+function renderArrayToCanvas(arr){
   var imgdata = ctx.getImageData(0,0, width, height);
   for(var i=0; i<width; i++){
     for(var j=0; j < height; j++){
       var index = j*width + i;
-      var depth = dataArray[i][j]/5;
+      var depth = arr[i][j];
       imgdata.data[4*index] = depth;
       imgdata.data[4*index+1] = depth;
       imgdata.data[4*index+2] = depth;
@@ -35,6 +35,42 @@ function renderCanvas(dataArray){
     }
   }
   ctx.putImageData(imgdata,0,0);
+}
+
+function processArray(arr){
+  var res = createArray(width,height);
+  for(var i=0; i< width; i++){
+    for(var j=0; j < height-1; j++){
+      if(arr[i][j]-arr[i][j+1]<0.5){
+        res[i][j] = 200;
+      }
+    }
+  }
+  return res;
+}
+
+function initThreeJS() {
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.z = 10;
+    scene = new THREE.Scene();
+    for(var i=0; i<10; i++){
+      for(var j=0; j<10; j++){
+        var geometry = new THREE.CubeGeometry(1,1,1);
+        var material = new THREE.MeshBasicMaterial( { color: 0xff0000} );
+        var mesh = new THREE.Mesh( geometry, material );
+        scene.add( mesh );
+        vertices.push( new THREE.Vector3(i, j, i ));
+      }
+    }
+    //geometry = new THREE.Geometry();
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight);
+    $("#container").append( renderer.domElement );
+}
+
+function renderThreeJS(){
+    requestAnimationFrame(renderThreeJS);
+    renderer.render( scene, camera );
 }
 
 
@@ -46,19 +82,24 @@ var width = 640;
 var height = 480;
 var ctx = document.getElementById('canvas').getContext('2d');
 
+var camera, scene, renderer;
+var geometry, material, mesh;
+var vertices = [];
+
+//initThreeJS();
+//renderThreeJS();
 //loop
+
 socket.on('data', function (data) {
   var bytearray = new Uint8Array(data);
   var dataArray = getDataArray(bytearray);
-  renderCanvas(dataArray);
+  renderArrayToCanvas(processArray(dataArray));
 });
+
 
 socket.on('end', function(){
   console.log("stream ended");
 });
-
-
-
 
 },{"websocket-stream":2}],3:[function(require,module,exports){
 var events = require('events');
